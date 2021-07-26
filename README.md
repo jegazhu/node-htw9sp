44eb59b0f3b3ddaeb88c076c4c29f8d3ee733a13
jegazhu <jegazhu@gmail.com>
2021-07-26 16:15:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-htw9sp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-htw9sp)
---
+++ netlifty-form/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Get in touch </h1>
+      <Form />
+    </div>
+  );
+}
+
+
+export default App;
